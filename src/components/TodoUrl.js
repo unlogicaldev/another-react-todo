4c672eb6f5ecbe31d7cabae4afa5a3fc84af2ac2
YONGHYUK LEE <yonghyuklee@YONGHYUKui-MacBook.local>
2019-06-29 13:43:55 +0900

    TodoUrl component

--- src/components/TodoUrl.js
+++ src/components/TodoUrl.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-import { Card } from 'antd';
+import { Card, Spin } from 'antd';
 class TodoUrl extends Component {
 
   render() {
     const { url } = this.props;
     return (
       <div>
-        <Card title="제목" extra={<a href={url} target="_blank">More</a>} style={{ width: 300 }}>
-            <p>이미지</p>
-            <p>설명</p>
-            <p>{url}</p>
+        <Card title={url} extra={<a href={url} target="_blank">More</a>} style={{ width: 300 }}>
+          <Spin size="large" />
         </Card>
       </div>
     );
